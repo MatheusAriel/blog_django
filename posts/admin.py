8628afafd5d4ac12d45470ab8dbff7483b24c8e1
Matheus Ariel <matheusariel@gmail.com>
2022-07-31 04:50:15 -0300

    Commit

--- posts/admin.py
+++ posts/admin.py
@@ -9,7 +9,7 @@ class PostAdmin(SummernoteModelAdmin):
     list_display = ('id', 'titulo_post', 'autor_post', 'data_post', 'categoria_post', 'publicacao_post')
     list_editable = ('categoria_post', 'publicacao_post')
     list_display_links = ('id', 'titulo_post')
-    summernote_fields = ('conteudo_post', 'excerto_post')
+    summernote_fields = ('conteudo_post')
 
 
 admin.site.register(Post, PostAdmin)
--- posts/views.py
+++ posts/views.py
@@ -2,13 +2,26 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from posts.models import Post
+from django.db.models import Q, Count, Case, When
 
 
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
-    paginate_by = 3
+    paginate_by = 3  # 6
     context_object_name = 'posts'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('data_post').filter(publicacao_post=True)
+        qs = qs.annotate(
+            numero_comentarios=Count(
+                Case(
+                    When(comentario__publicado_comentario=True, then=1)
+                )
+            )
+        )
+        return qs
 
 
 class PostBusca(PostIndex):
